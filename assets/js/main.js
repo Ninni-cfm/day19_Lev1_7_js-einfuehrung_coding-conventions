1416c86fac5d79e9e6eb6a94cf4d3761861fbacb
Ninni-cfm <k.nienhaus@cfm-net.de>
2021-07-15 13:42:08 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+/*
+    Naming
+    In general, use functionNamesLikeThis, variableNamesLikeThis, ClassNamesLikeThis,
+    EnumNamesLikeThis, methodNamesLikeThis, CONSTANT_VALUES_LIKE_THIS, 
+    foo.namespaceNamesLikeThis.bar, and filenameslikethis.js
+*/
+
+// simple string
+let helloWorld = "Hello World";
+
+// pi will never change its value!
+const PI = 3.14;
+
+// string because of the unit (meters) after the value
+let burjDubai = "828m";
+
+// I don't think the height of the building will change...
+const BURJ_DUBAI = "828m";
+
+// simple string
+let fullName = "Jan Schmidt";
+
+// string because of the unit (meters) after the value
+let eiffelTower = "324m";
+
+// I don't think the height of the building will change...
+const EIFFEL_TOWER = "324m";
+
+// sample for camelCase
+let camelCase = "camelCase";
+
+// simple boolean
+let kursStatus = true;
